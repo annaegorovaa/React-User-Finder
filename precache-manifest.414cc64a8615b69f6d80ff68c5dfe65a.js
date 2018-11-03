fe1500d430ab88786e86ed06ca1b599f376645bb
annaegorovaa <anju.egorova@gmail.com>
2018-11-03 14:52:26 -0700

    Updates

---
+++ precache-manifest.414cc64a8615b69f6d80ff68c5dfe65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82788b956b54b677f320",
+    "url": "/React-User-Finder/static/js/runtime~main.82788b95.js"
+  },
+  {
+    "revision": "420d6126620e91d7bc02",
+    "url": "/React-User-Finder/static/js/main.420d6126.chunk.js"
+  },
+  {
+    "revision": "3fe64d2d6c84ff7c2ef3",
+    "url": "/React-User-Finder/static/js/1.3fe64d2d.chunk.js"
+  },
+  {
+    "revision": "420d6126620e91d7bc02",
+    "url": "/React-User-Finder/static/css/main.eecdb09b.chunk.css"
+  },
+  {
+    "revision": "2267c756cc9d3980b3a332a223a9af04",
+    "url": "/React-User-Finder/index.html"
+  }
+];
